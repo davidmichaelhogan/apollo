6ac1d783b60403ca6be4c8874db593f9704a004b
cr-ks <davidmichaelandco@gmail.com>
2019-11-13 16:37:27 -0500

    cleaning container components

--- meta/dashtester/src/index.js
+++ meta/dashtester/src/index.js
@@ -1,3 +1,9 @@
+// ------------------------------------------------------//
+// App Index File | Starter Dashboard
+// Apollo V2
+// David Michael Hogan | November 13, 2019 | Updated:
+// ------------------------------------------------------//
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,4 +12,5 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// not using service workers atm, view serviceWorker.js for more info
 serviceWorker.unregister();
