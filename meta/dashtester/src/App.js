a3b33bc5f464caa517b5af17c460d80b90c4b9e1
cr-ks <davidmichaelandco@gmail.com>
2019-11-13 16:09:51 -0500

    started cleaning App.js

--- meta/dashtester/src/App.js
+++ meta/dashtester/src/App.js
@@ -1,27 +1,22 @@
+//-------------------------------------------------------------//
+//
+//
+//
+//-------------------------------------------------------------//
+
 import React, { Component } from 'react';
+
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { Chart } from 'react-chartjs-2';
-import { ThemeProvider } from '@material-ui/styles';
-import validate from 'validate.js';
-
-import { chartjs } from './helpers';
-import theme from './theme';
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import './assets/scss/index.scss';
-import validators from './common/validators';
 import Routes from './Routes';
 
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
+
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import './assets/scss/index.scss';
+
 const browserHistory = createBrowserHistory();
-
-Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
-  draw: chartjs.draw
-});
-
-validate.validators = {
-  ...validate.validators,
-  ...validators
-};
 
 export default class App extends Component {
   render() {
@@ -34,3 +29,25 @@ export default class App extends Component {
     );
   }
 }
+
+//-------------------------------------------------------------//
+// creates rounded corners on bar graph : dh 11/13/19
+//-------------------------------------------------------------//
+// import validate from 'validate.js';
+// import validators from './common/validators';
+// validate.validators = {
+//   ...validate.validators,
+//   ...validators
+// };
+//-------------------------------------------------------------//
+
+
+//-------------------------------------------------------------//
+// creates rounded corners on bar graph : dh 11/13/19
+//-------------------------------------------------------------//
+// import { Chart } from 'react-chartjs-2';
+// import { chartjs } from './helpers';
+// Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+//   draw: chartjs.draw
+// });
+//-------------------------------------------------------------//
